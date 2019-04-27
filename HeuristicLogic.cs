49e9fe9b0fcb02fd71a7350a5e8d5e209f4467d0
MichaelS-W <41055144+MichaelS-W@users.noreply.github.com>
2019-04-27 17:47:02 +0100

    Add files via upload
    WaypointFollower Scripts

---
+++ HeuristicLogic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+public class HeuristicLogic : Decision
+{
+    public override float[] Decide(List<float> vectorObs,List<Texture2D> visualObs,float reward,bool done,List<float> memory)
+    {
+        float[] act = new float[brainParameters.vectorActionSize.Length];
+        if (vectorObs[1] > 2.5f && vectorObs[0] == 1f)
+        {
+            act[0] = 2f;
+        }
+        else if (vectorObs[1] < -2.5f && vectorObs[0] == 0f)
+        {
+            act[0] = 2f;
+        }
+        else if (vectorObs[1] > 2.5f && vectorObs[0] == 0f)
+        {
+            act[0] = 1f;
+        }
+        else if (vectorObs[1] < -2.5f && vectorObs[0] == 1f)
+        {
+            act[0] = 1f;
+        }
+        else
+        {
+            act[0] = 0f;
+        }
+        return act;
+        }
+
+
+    public override List<float> MakeMemory(
+        List<float> vectorObs,
+        List<Texture2D> visualObs,
+        float reward,
+        bool done,
+        List<float> memory)
+    {
+        return new List<float>();
+    }
+}
